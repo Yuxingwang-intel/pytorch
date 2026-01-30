d7870121676cdcc6adce90da74dcff545cd048af
Yuxingwang-intel <yuxing.wang@intel.com>
2026-01-31 01:08:12 +0800

    Updates for rebase

--- torch/_inductor/codegen/cpp_gemm_template.py
+++ torch/_inductor/codegen/cpp_gemm_template.py
@@ -413,19 +413,19 @@ def expand_bias(B: Optional[_T], X: _T) -> Optional[_T]:
     """
     Expand Bias to the same size of X.
     """
-    if B is not None and len(B.shape) == 1:
+    if B is not None:
         if isinstance(B, ir.IRNode):
             if not isinstance(B, ir.TensorBox):
                 # pyrefly: ignore [bad-assignment]
                 B = ir.TensorBox(B)
             assert hasattr(X, "get_size")
-            # pyrefly: ignore [missing-attribute]
-            B = L.expand(B, (X.get_size()[0], B.get_size()[-1]))
+            if len(B.get_size()) == 1:
+                B = L.expand(B, (X.get_size()[0], B.get_size()[-1]))
         else:
             assert isinstance(B, torch.Tensor)
             assert isinstance(X, torch.Tensor)
-            # pyrefly: ignore [bad-assignment]
-            B = B.expand(X.shape[0], B.shape[-1])
+            if len(B.shape) == 1:
+                B = B.expand(X.shape[0], B.shape[-1])
     return B
 
 
