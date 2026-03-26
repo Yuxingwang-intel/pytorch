10e3cac0dff6255288f31e908424da1fe3c92ae1
Yuxingwang-intel <yuxing.wang@intel.com>
2026-03-26 23:48:43 +0800

    Update formatting

--- torch/_inductor/fx_passes/pre_grad.py
+++ torch/_inductor/fx_passes/pre_grad.py
@@ -434,18 +434,17 @@ def remove_identity(gm: torch.fx.GraphModule) -> torch.fx.GraphModule:
     graph = gm.graph
     work_done = False
     for module_name, module in gm.named_modules():
-       if type(module) is nn.Identity:
-           for node in graph.find_nodes(op="call_module", target=module_name):
-              assert len(node.args) == 1
-              input_node = node.args[0]
-              node.replace_all_uses_with(input_node)
-              graph.erase_node(node)
-              work_done = True
+        if type(module) is nn.Identity:
+            for node in list(graph.find_nodes(op="call_module", target=module_name)):
+                assert len(node.args) == 1
+                input_node = node.args[0]
+                node.replace_all_uses_with(input_node)
+                graph.erase_node(node)
+                work_done = True
     if work_done:
         graph.lint()
         gm.recompile()
     return gm
-
 
 def fuse_conv_bn(gm: torch.fx.GraphModule, inplace=False) -> torch.fx.GraphModule:
     """
